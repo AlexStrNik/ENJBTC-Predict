012c29ab833de99bc4a8b021166fcc70e2699256
alexa <alexandrstrizjnev@gmail.com>
2018-03-29 18:12:04 +0300

    qqq

---
+++ app/main/routes.py
@@ -0,0 +1,26 @@
+from flask import session, redirect, url_for, render_template, request
+from flask_socketio import emit, join_room, leave_room
+from . import main
+import json
+from .. import socketio
+
+room = 'QWERTY'
+prices = []
+
+
+@main.route('/getPrice')
+def get_price():
+    return json.dumps(prices)
+
+
+@main.route('/', methods=['POST'])
+def post():
+    print(request.json)
+    join_room(room)
+    emit('message', {'msg': session.get('name') + ' has entered the room.'}, room=room)
+
+
+@main.route('/', methods=['GET'])
+def index():
+    """Login form to enter a room."""
+    return render_template('index.html')
